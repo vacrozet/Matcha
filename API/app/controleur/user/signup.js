bfb7d241b0706bc54f5f5febb1b8afdd86f12001
vacrozet <vacrozet@student.42.fr>
2017-10-12 16:16:14 +0200

    patch

--- API/app/controleur/user/signup.js
+++ API/app/controleur/user/signup.js
@@ -72,6 +72,11 @@ module.exports = (req, res) => {
   }
   if (req.body.date === undefined || !req.body.date.match(/[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])/)) {
     return errorform(res, false, 'date incorrect')
+  } else {
+    let test = getAge(req.body.date)
+    if (test < 18 || test > 99) {
+      return errorform(res, false, 'vous navez pas 18 ans')
+    }
   }
   if (req.body.bio === undefined) {
     return errorform(res, false, 'bio not defined')
